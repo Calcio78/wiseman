dc5416d327cd9d2225e4d65270b926ee85bf933b
Shem Kua <mozart_sk8@Shems-MacBook-Pro.local>
2016-04-21 03:10:26 -0500

    Initial commit

---
+++ wise-editor/src/main/webapp/js/less.js
@@ -0,0 +1,1 @@
+../../../../../mindplot/src/main/javascript/libraries/less/less-1.4.2.min.js
---
+++ wise-webapp/src/main/webapp/js/mootools-core.js
@@ -0,0 +1,1 @@
+../../../../../mindplot/src/main/javascript/libraries/mootools/mootools-core-1.4.5-full-nocompat-yc.js
